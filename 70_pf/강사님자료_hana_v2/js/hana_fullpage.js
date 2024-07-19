c7e86b558b48bf53aeb4a9536884d56d5e7a228e
12alsrud12 <12alsrud12@naver.com>
2024-07-19 12:09:49 +0900

    d
    d

---
+++ 70_pf/강사님자료_hana_v2/js/hana_fullpage.js
@@ -0,0 +1,9 @@
+// hana full page.js
+
+window.addEventListener("DOMContentLoaded", function () {
+    new fullpage('#fullpage', {
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
+});
