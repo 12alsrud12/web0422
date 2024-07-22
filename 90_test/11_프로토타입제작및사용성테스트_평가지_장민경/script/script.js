5bbf8166f9d30098e8a10022b743b40055151e80
12alsrud12 <12alsrud12@naver.com>
2024-07-22 09:59:23 +0900

    d
    d

---
+++ 90_test/11_프로토타입제작및사용성테스트_평가지_장민경/script/script.js
@@ -0,0 +1,54 @@
+const n1 = document.querySelector("#pp p");
+
+window.addEventListener("scroll", function () {
+    n1.innerHTML = Math.floor(pageYOffset);
+    n1.innerHTML = Math.floor(pageYOffset);
+});
+
+console.log(n1);
+
+$('.slide').bxSlider({
+    // General
+    mode: 'fade',
+    // auto를 false 로 바꾸면 캐러샐 슬라이드 동작 멈춤 버튼표시를 안보이게+자동슬라이드 정지.
+    auto: true,
+    // autoControls는 false로 하면 위와 다르게 슬라이드는 움직이고 버튼만 지운다.
+    autoControls: false,
+    // click시 자동 슬라이드를 순간 정지시킨다. false로 변경하면 click해도 슬라이드가 안멈춘다.
+    stopAutoOnClick: false,
+    // 페이저(pager,indicator 인디케이터 라고도함) 은 슬라이드 순서 보여주는 동그라미
+    pager: false,
+    // pause : 슬라이드 정지 시간 1밀리미초라서 1000 = 1초 4000 = 4초
+    pause: 5000,
+    slideWidth: 1080
+});
+
+$('.slide2').bxSlider({
+    // General
+    mode: 'fade',
+    // auto를 false 로 바꾸면 캐러샐 슬라이드 동작 멈춤 버튼표시를 안보이게+자동슬라이드 정지.
+    auto: true,
+    // autoControls는 false로 하면 위와 다르게 슬라이드는 움직이고 버튼만 지운다.
+    autoControls: false,
+    // click시 자동 슬라이드를 순간 정지시킨다. false로 변경하면 click해도 슬라이드가 안멈춘다.
+    stopAutoOnClick: false,
+    // 페이저(pager,indicator 인디케이터 라고도함) 은 슬라이드 순서 보여주는 동그라미
+    pager: false,
+    // pause : 슬라이드 정지 시간 1밀리미초라서 1000 = 1초 4000 = 4초
+    pause: 5000,
+    slideWidth: 1920
+});
+
+const header = document.querySelector("header");
+const serch = document.querySelectorAll(".serch a")[0];
+const myPage = document.querySelectorAll(".serch_my>a")[0];
+
+header.onmouseenter = function () {
+    serch.innerHTML = ('<img src="images/header/serch_ico_hover.png" alt="hover_ico1">');
+    myPage.innerHTML = ('<img src="images/header/My_page_ico_hover.png" alt="hover_ico1">');
+};
+
+header.onmouseleave = function () {
+    serch.innerHTML = ('<img src="images/header/serch_ico.png" alt="hover_ico1">');
+    myPage.innerHTML = ('<img src="images/header/My_page_ico.png" alt="hover_ico1">');
+};
