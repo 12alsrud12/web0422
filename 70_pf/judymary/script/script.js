5ff731c2b7857189a3bf88a6c49bb7d2ffc30f58
12alsrud12 <12alsrud12@naver.com>
2024-08-01 15:26:29 +0900

    d
    d

--- 70_pf/judymary/script/script.js
+++ 70_pf/judymary/script/script.js
@@ -1,3 +1,19 @@
-window.addEventListener("DomContentLoaded", function () {
+window.addEventListener("DomContentLoaded", () => {
+
+    // 요소찾기
+    const header = document.querySelector("header");
+    const nav = document.querySelector("nav");
+    const x = document.querySelectorAll("header .x_logo")[0];
+    console.log(header);
+    console.log(nav);
+    console.log(x);
+
+    x.onclick = function () {
+        if (x === classList('active')) {
+            x.classList.remove('active');
+        } else {
+            x.classList.add('active');
+        };
+    };
 
 });
