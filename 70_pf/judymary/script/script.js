c52f416c2c9158bd190e237d04b91d5a8bbf170b
12alsrud12 <12alsrud12@naver.com>
2024-08-03 20:24:25 +0900

    d
    d

--- 70_pf/judymary/script/script.js
+++ 70_pf/judymary/script/script.js
@@ -1,19 +1,34 @@
-window.addEventListener("DomContentLoaded", () => {
-
+window.addEventListener("DOMContentLoaded", () => {
     // 요소찾기
-    const header = document.querySelector("header");
-    const nav = document.querySelector("nav");
-    const x = document.querySelectorAll("header .x_logo")[0];
-    console.log(header);
-    console.log(nav);
-    console.log(x);
 
-    x.onclick = function () {
-        if (x === classList('active')) {
-            x.classList.remove('active');
-        } else {
-            x.classList.add('active');
-        };
+    // sec3 contents
+    // sec3 button
+    const bt1 = document.querySelectorAll(".btn li")[0];
+    const bt2 = document.querySelectorAll(".btn li")[1];
+    console.log(bt1);
+    console.log(bt2);
+    // sec3 tabs
+    const tab1 = document.querySelectorAll(".tab")[0];
+    const tab2 = document.querySelectorAll(".tab")[1];
+    console.log(tab1);
+    console.log(tab2);
+
+    // tab reset
+    function resetA() {
+        bt1.classList.remove("on");
+        bt2.classList.remove("on");
+        tab1.classList.remove("on");
+        tab2.classList.remove("on");
     };
 
-});
+    bt1.onclick = function () {
+        resetA();
+        bt1.classList.add("on");
+        tab1.classList.add("on");
+    };
+    bt2.onclick = function () {
+        resetA();
+        bt2.classList.add("on");
+        tab2.classList.add("on");
+    };
+})
