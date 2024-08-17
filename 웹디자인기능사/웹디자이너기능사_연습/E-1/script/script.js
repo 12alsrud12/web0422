dd384499eedbb8a486d94a57870be73214ddebd0
12alsrud12 <12alsrud12@naver.com>
2024-08-17 20:55:39 +0900

    웹디기능사연습

--- 웹디자인기능사/웹디자이너기능사_연습/E-1/script/script.js
+++ 웹디자인기능사/웹디자이너기능사_연습/E-1/script/script.js
@@ -3,7 +3,6 @@ window.addEventListener("DOMContentLoaded", () => {
     // 요소찾기
     // 팝업창 부분
     const modal = document.querySelector("#modal");
-    const popup = document.querySelector("#popup");
     const exit = document.querySelector("#popup button")
     // 공지사항 1번째 부분
     const btn1 = document.querySelectorAll("#notion ul li")[0];
