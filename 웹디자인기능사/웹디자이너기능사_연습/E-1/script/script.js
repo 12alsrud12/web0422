542a82a18e6751883e01470f2906847ae59c2ed8
12alsrud12 <12alsrud12@naver.com>
2024-08-17 20:15:00 +0900

    12

--- 웹디자인기능사/웹디자이너기능사_연습/E-1/script/script.js
+++ 웹디자인기능사/웹디자이너기능사_연습/E-1/script/script.js
@@ -1,3 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // 모달팝업
+    // 요소찾기
+    // 팝업창 부분
+    const modal = document.querySelector("#modal");
+    const popup = document.querySelector("#popup");
+    const exit = document.querySelector("#popup button")
+    // 공지사항 1번째 부분
+    const btn1 = document.querySelectorAll("#notion ul li")[0];
 
+    // 이벤트
+    btn1.onclick = () => {
+        modal.classList.add("on");
+    };
+
+    exit.onclick = () => {
+        modal.classList.remove("on");
+    };
 });
