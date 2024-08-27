ea429eecd117699da81e48a2bfa9916236b1a5f8
12alsrud12 <12alsrud12@naver.com>
2024-08-27 17:28:47 +0900

    asd

--- 70_pf/team2/04.js/header.js
+++ 70_pf/team2/04.js/header.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // 헤더 PC
     const pMath = document.querySelector("#mathfoor");
     const headerPc = document.querySelector("header.pc");
 
@@ -11,4 +12,39 @@ window.addEventListener("DOMContentLoaded", () => {
             headerPc.classList.remove("down");
         };
     });
+
+    // 헤더 모바일, 태블릿
+    // header serch
+    // 요소찾기
+    const mb_header = document.querySelector("header.mb.tb");
+    const serch = document.querySelector("header.mb.tb .serch>div");
+    const serch_input = document.querySelector("header.mb.tb .serch>input");
+    console.log(mb_header);
+    console.log(serch);
+    console.log(serch_input);
+
+    // 마우스 클릭 시 검색창 생성
+    serch.onclick = () => {
+        serch_input.classList.add("active")
+    };
+
+    // 마우스 떠낫을 시 검색창 사라짐
+    serch.onmouseleave = () => {
+        serch_input.classList.remove("active")
+    };
+
+    // x_logo
+    const x = document.querySelector("#x_logo");
+    const nav = document.querySelector("header.mb.tb>nav");
+    console.log(x);
+    console.log(nav);
+
+    function xEvent() {
+        x.classList.toggle("active");
+        nav.classList.toggle("active");
+    };
+
+    x.onclick = () => {
+        xEvent();
+    };
 });
--- 70_pf/team2/04.js/index.js
+++ 70_pf/team2/04.js/index.js
@@ -1,36 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-    // header serch
-    // 요소찾기
-    const mb_header = document.querySelector("header.mb.tb");
-    const serch = document.querySelector("header.mb.tb .serch>div");
-    const serch_input = document.querySelector("header.mb.tb .serch>input");
-    console.log(mb_header);
-    console.log(serch);
-    console.log(serch_input);
+    // section1 요소찾기
+    const x_button = document.querySelector(".x_button");
+    const sec1_sub1 = document.querySelector("#sec1_wrap");
+    const sec1_sub2 = document.querySelector("#s1_sub_cont");
 
-    // 마우스 클릭 시 검색창 생성
-    serch.onclick = () => {
-        serch_input.classList.add("active")
-    };
+    // x버튼을 click시 s1_banner 3개가 등장한다.
 
-    // 마우스 떠낫을 시 검색창 사라짐
-    serch.onmouseleave = () => {
-        serch_input.classList.remove("active")
-    };
-
-    // x_logo
-    const x = document.querySelector("#x_logo");
-    const nav = document.querySelector("header.mb.tb>nav");
-    console.log(x);
-    console.log(nav);
-
-    function xEvent() {
-        x.classList.toggle("active");
-        nav.classList.toggle("active");
-    };
-
-    x.onclick = () => {
-        xEvent();
+    x_button.onclick = () => {
+        sec1_sub1.classList.toggle("action");
+        sec1_sub2.classList.toggle("action");
     };
 
 });
