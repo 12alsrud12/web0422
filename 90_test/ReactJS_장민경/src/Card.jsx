da5f691dfc00ec8dddb3cbe5bee544d472bf9669
12alsrud12 <12alsrud12@naver.com>
2024-09-02 11:08:00 +0900

    17-test

--- 90_test/ReactJS_장민경/src/Card.jsx
+++ 90_test/ReactJS_장민경/src/Card.jsx
@@ -7,9 +7,9 @@
     uiDetails: Detauls = 세부/ ui 카드의 세부 정보,
     uiPrice: Price = 가격/ ui카드의 가격 부분
 */
-const UiCard = [
+const uiCard = [
     {
-        uiImg: "ui-card-img1.png",
+        uiImg: "../ui-card-img1.png",
         uiImgNum: "UI_카드_이미지1",
         uiTitle: "라온",
         uiLike: 5,
@@ -18,7 +18,7 @@ const UiCard = [
         uiPrice: "100,000원"
     },
     {
-        uiImg: "ui-card-img2.png",
+        uiImg: "../ui-card-img2.png",
         uiImgNum: "UI_카드_이미지2",
         uiTitle: "가평 체리쉬애견팬션",
         uiLike: 0,
@@ -27,7 +27,7 @@ const UiCard = [
         uiPrice: "39,000원"
     },
     {
-        uiImg: "ui-card-img3.png",
+        uiImg: "../ui-card-img3.png",
         uiImgNum: "UI_카드_이미지3",
         uiTitle: "도그팰리스 프리미엄 독채 글램핑",
         uiLike: 5,
@@ -36,14 +36,14 @@ const UiCard = [
         uiPrice: "205,000원"
     },
     {
-        uiImg: "ui-card-img4.png",
+        uiImg: "../ui-card-img4.png",
         uiImgNum: "UI_카드_이미지4",
         uiTitle: "경주 마법의성 애견펜션",
         uiLike: "4.9",
         uiSubLike: "(19)",
         uiDetails: "경북 경주시 · ~10kg · 소형견 · 애견수영장 · 애견놀이터",
         uiPrice: "70,000원"
-    },
+    }
 ]
 
-// export default UiCard;
+export default uiCard;
