6f16b5b3280b00ccc37345d6d17ee1c4c6e34cb8
12alsrud12 <12alsrud12@naver.com>
2024-09-02 13:07:55 +0900

    team2

--- 70_pf/team2/04.js/index.js
+++ 70_pf/team2/04.js/index.js
@@ -13,44 +13,45 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // section2
 
-    // section3
-    $(() => {
-        $('.sec3_slider').slick({
-            // 자동슬라이드
-            autoplay: true,
-            // 슬라이드 속도 2000 = 2초
-            autoplaySpeed: 2000,
-            // 슬라이드 시 이미지 몇개를 넘길것인지 = 1일시 1개씩 이동
-            slidesToScroll: 1,
-            // 현재 보여지는 슬라이드 수
-            slidesToShow: 3,
-            // 현재 이미지 중앙에 배치
-            centerMode: true,
-            centerPadding: '40px',
-            // responsive = 반응형
-            responsive: [
-                {
-                    // breakpoint = width가 1025보다 작을 때 작동 / 1025이하 테블릿, 모바일
-                    breakpoint: 1025,
-                    settings: {
-                        // arrows: false,
-                        centerMode: true,
-                        centerPadding: '56px',
-                        slidesToShow: 1,
-                    }
-                },
-                {
-                    // breakpoint = width가 376보다 작을 때 작동 / 376이하 모바일
-                    breakpoint: 376,
-                    settings: {
-                        // arrows: false,
-                        centerMode: true,
-                        centerPadding: '24px',
-                        slidesToShow: 1
-                    }
+
+// section3
+$(() => {
+    $('.sec3_slider').slick({
+        // 자동슬라이드
+        autoplay: true,
+        // 슬라이드 속도 2000 = 2초
+        autoplaySpeed: 2000,
+        // 슬라이드 시 이미지 몇개를 넘길것인지 = 1일시 1개씩 이동
+        slidesToScroll: 1,
+        // 현재 보여지는 슬라이드 수
+        slidesToShow: 3,
+        // 현재 이미지 중앙에 배치
+        centerMode: true,
+        centerPadding: '40px',
+        // responsive = 반응형
+        responsive: [
+            {
+                // breakpoint = width가 1025보다 작을 때 작동 / 1025이하 테블릿, 모바일
+                breakpoint: 1025,
+                settings: {
+                    // arrows: false,
+                    centerMode: true,
+                    centerPadding: '56px',
+                    slidesToShow: 1,
                 }
-            ]
-        });
-    }); // Loaded Event
+            },
+            {
+                // breakpoint = width가 376보다 작을 때 작동 / 376이하 모바일
+                breakpoint: 376,
+                settings: {
+                    // arrows: false,
+                    centerMode: true,
+                    centerPadding: '24px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+}); // Loaded Event
 
 });
