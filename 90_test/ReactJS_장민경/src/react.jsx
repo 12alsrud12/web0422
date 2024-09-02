1d8550b79764cc88ffc415773537626ed1b64caf
12alsrud12 <12alsrud12@naver.com>
2024-09-02 10:24:05 +0900

    17-test

--- 90_test/ReactJS_장민경/src/react.jsx
+++ 90_test/ReactJS_장민경/src/react.jsx
@@ -1,10 +1,64 @@
-import { UiCard } from "Card.jsx"
+/* 
+    uiImg: img src 태그에 넣을 text,
+    uiImgNum: img alt 태그에 넣을 text,
+    uiTitle: UI 카드의 제목 부분,
+    uiLike: UI 카드 좋아요 별점 평균 부분,
+    uiSubLike: 사용자 몇명이 리뷰를 남겼는가를 알려주는 text,
+    uiDetails: Detauls = 세부/ ui 카드의 세부 정보,
+    uiPrice: Price = 가격/ ui카드의 가격 부분
+*/
+const UiCard = [
+    {
+        uiImg: "ui-card-img1.png",
+        uiImgNum: "UI_카드_이미지1",
+        uiTitle: "라온",
+        uiLike: 5,
+        uiSubLike: "(2)",
+        uiDetails: "부산 수영구 · 모든견종 · 무선인터넷 · 엘리베이터",
+        uiPrice: "100,000원"
+    },
+    {
+        uiImg: "ui-card-img2.png",
+        uiImgNum: "UI_카드_이미지2",
+        uiTitle: "가평 체리쉬애견팬션",
+        uiLike: 0,
+        uiSubLike: "(0)",
+        uiDetails: "경기 가평군 · ~20kg · 소형견 · 중형견 · 애견수영장",
+        uiPrice: "39,000원"
+    },
+    {
+        uiImg: "ui-card-img3.png",
+        uiImgNum: "UI_카드_이미지3",
+        uiTitle: "도그팰리스 프리미엄 독채 글램핑",
+        uiLike: 5,
+        uiSubLike: "(13)",
+        uiDetails: "충남 당진시 · 모든견종 ·맹견가능 · 드라이룸",
+        uiPrice: "205,000원"
+    },
+    {
+        uiImg: "ui-card-img4.png",
+        uiImgNum: "UI_카드_이미지4",
+        uiTitle: "경주 마법의성 애견펜션",
+        uiLike: "4.9",
+        uiSubLike: "(19)",
+        uiDetails: "경북 경주시 · ~10kg · 소형견 · 애견수영장 · 애견놀이터",
+        uiPrice: "70,000원"
+    },
+]
 
+// React HTML작성
 function ReactUi() {
     return (
         <div className="ui-card">
-            <div className="ui-img"></div>
-            <div className="ui-bx"></div>
+            <div className="ui-img">
+                <img src="{uiImg}" alt="{uiImgNum}" />
+            </div>
+            <div className="ui-bx">
+                <h4>{uiTitle}</h4>
+                <p>{uiLike} <span>{uiSubLike}</span> </p>
+                <p>{uiDetails}</p>
+                <h3>{uiPrice}</h3>
+            </div>
         </div>
     );
 }
