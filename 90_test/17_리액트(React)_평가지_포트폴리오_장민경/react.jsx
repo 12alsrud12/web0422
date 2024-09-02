e07767845d67e4a66417d2821542117ad75cfca1
12alsrud12 <12alsrud12@naver.com>
2024-09-02 09:59:45 +0900

    17-test

--- 90_test/17_리액트(React)_평가지_포트폴리오_장민경/react.jsx
+++
@@ -1,5 +0,0 @@
-function ReactUi() {
-    return (
-        <div></div>
-    );
-}
---
+++ 90_test/ReactJS_장민경/react.jsx
@@ -0,0 +1,8 @@
+function ReactUi() {
+    return (
+        <div className="ui-card">
+            <div className="ui-img"></div>
+            <div className="ui-bx"></div>
+        </div>
+    );
+}
