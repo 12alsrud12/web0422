476d568d771c47b6c698e7c5c637096cd4079dbf
12alsrud12 <12alsrud12@naver.com>
2024-09-10 21:08:55 +0900

    ㅁㅇㄴ

--- 70_pf/TouslesJours/script/script.js
+++ 70_pf/TouslesJours/script/script.js
@@ -46,38 +46,21 @@ header.onmouseleave = function () {
     myPage.innerHTML = ('<img src="images/My_page_ico.png" alt="hover_ico1">');
 };
 
-// tabs!!
-
-// 요소찾기
-const li1 = document.querySelectorAll('#tabs ul li')[0];
-const li2 = document.querySelectorAll('#tabs ul li')[1];
-
-const bt1 = document.querySelectorAll('#tabs a')[0];
-const bt2 = document.querySelectorAll('#tabs a')[1];
-
-
-// tab 1,2 찾기
-const cont1 = document.querySelector('#tab1');
-const cont2 = document.querySelector('#tab2');
-
-// 확인
-console.log(cont1);
-console.log(cont2);
-
-
-// 이벤트처리
-// 버튼1을 클릭하면 p.on
-bt1.onclick = function () {
-    cont1.classList.add('on');
-    cont2.classList.remove('on');
-};
-
-// 버튼2을 클릭하면 p.off
-bt2.onclick = function () {
-    cont2.classList.add('on');
-    cont1.classList.remove('on');
-};
-
+// 수정후! 섹션4 탭에서 슬라이드로 변경 js
+$('.slide3').bxSlider({
+    // General
+    // auto를 false 로 바꾸면 캐러샐 슬라이드 동작 멈춤 버튼표시를 안보이게+자동슬라이드 정지.
+    auto: true,
+    // autoControls는 false로 하면 위와 다르게 슬라이드는 움직이고 버튼만 지운다.
+    autoControls: false,
+    // click시 자동 슬라이드를 순간 정지시킨다. false로 변경하면 click해도 슬라이드가 안멈춘다.
+    stopAutoOnClick: false,
+    // 페이저(pager,indicator 인디케이터 라고도함) 은 슬라이드 순서 보여주는 동그라미
+    pager: false,
+    // pause : 슬라이드 정지 시간 1밀리미초라서 1000 = 1초 4000 = 4초
+    pause: 5000,
+    slideWidth: 1920
+});
 
 // 원페이지 스크롤시 섹션이동 기능
 $(function () {
